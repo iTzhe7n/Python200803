4e1e24239db89d4b27f2d3ba655a70d1d905cf47
iTzhe7n <69131670+iTzhe7n@users.noreply.github.com>
2020-08-03 11:13:49 +0800

    Add files via upload

---
+++ day1-1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:05:19 2020
+
+@author: AE401
+"""
+print("I'M Eric")
+print('He is "cooL"')
+print("I'M Eric \nHe is \"cooL\"")
---
+++ day1-2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 10:27:41 2020
+
+@author: AE401
+"""
+name=input("請問你的名子")
+print("HI"+name)
