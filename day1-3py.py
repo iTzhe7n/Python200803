5c285f0f7311f6fcb0a833036c26a3a134b72739
iTzhe7n <69131670+iTzhe7n@users.noreply.github.com>
2020-08-03 13:56:16 +0800

    Add files via upload

---
+++ day1-3py.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 11:23:47 2020
+
+@author: AE401
+"""
+n=input("半徑: ")
+
+o=2*3.14*float(n)
+a=3.14*float(n)**2
+
+print('圓周 :',o)
+print('園面積 :',a)
