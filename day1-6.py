4d6e1cb7d76197466ab5e8d08ede62beab952d98
iTzhe7n <69131670+iTzhe7n@users.noreply.github.com>
2020-08-03 16:14:53 +0800

    Add files via upload

--- day1-6.py
+++ day1-6.py
@@ -1,21 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug  3 15:00:14 2020
+Created on Mon Aug  3 15:18:44 2020
 
 @author: AE401
 """
-b=input("體重: ")
-h=input("身高:")
-bmi=b//float(h)**2
-if bmi >=18.5:
-        print('體重不足')
-elif bmi >=24:
-        print('正常')
-elif bmi >=27:
-        print('過重')
-elif bmi >=30:
-        print('輕度肥胖')
-elif bmi >=35:
-        print('中度肥胖')
-else:
-        print(重度肥胖)
+m=int(input("數學成績:"))
+e=int(input("英文成績:"))
+if m >=90 and e >=90:
+    print('有獎品')
+elif m <60 and e <60:
+    print('處罰')
+elif m<=60 or e <=60:
+    print('再加油')
